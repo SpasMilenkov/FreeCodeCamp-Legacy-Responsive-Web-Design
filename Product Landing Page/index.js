15f87d05a8540d5cf1d99d3a64cd3863b5d49664
Clash2453 <spasmilenkov123@gmail.com>
2022-06-30 19:14:16 +0300

    refactored the code and made performance improvements

--- Product Landing Page/index.js
+++ Product Landing Page/index.js
@@ -5,39 +5,24 @@
 
 const targets = document.querySelectorAll("h1");
 const cards =  [...document.getElementsByClassName("card")];
-//all h1s count mainly used for the first header in the promo element
-const headerTransition = target =>{
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if(entry.isIntersecting){
-                const element = entry.target;
-                if(element.classList.contains("card")){
-                    element.classList.add("enterRight");
-                    observer.disconnect();
-                }
-                element.classList.add("enterBelow");
-                observer.disconnect();
-            };
+const container = document.getElementById("offers-grid");
+const offers = [...container.children];
+
+function animations(animationType, arr){
+    const transition = target =>{
+        const observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if(entry.isIntersecting){
+                    const element = entry.target;
+                        element.classList.add(animationType);
+                        observer.disconnect();
+                };
+            });
         });
-    });
-    observer.observe(target);
-};
-//looks for the cards in the second part
-const cardTransition = target =>{
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if(entry.isIntersecting){
-                const element = entry.target;
-                if(element.id=== "card2"){
-                    element.classList.add("enterLeft");
-                    observer.disconnect();
-                }
-                element.classList.add("enterRight");
-                observer.disconnect();
-            };
-        });
-    });
-    observer.observe(target);
-};
-targets.forEach(headerTransition);
-cards.forEach(cardTransition);
+        observer.observe(target);
+    };
+    arr.forEach(transition)
+}
+// targets.forEach(headerTransition);
+animations("enterLeft", cards);
+animations("enterBelow", targets);
